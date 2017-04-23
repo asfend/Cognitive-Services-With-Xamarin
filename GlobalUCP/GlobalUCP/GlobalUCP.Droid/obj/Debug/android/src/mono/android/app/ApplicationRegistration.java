3877afeb8b09f3b123fd2695bf4356fc2254e9ce
DESKTOP-N17J10J\Asfend <asfend@smashcloudmedia.com>
2017-04-23 18:59:36 +0500

    Microsoft Cognitive Service with Xamarin In Azure Global BootCamp

---
+++ GlobalUCP/GlobalUCP/GlobalUCP.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("GlobalUCP.Droid.MainApplication, GlobalUCP.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5478c118f5272846442cb64820e4763a9.MainApplication.class, md5478c118f5272846442cb64820e4763a9.MainApplication.__md_methods);
+		
+	}
+}
---
+++ GlobalUCP/GlobalUCP/GlobalUCP/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Vision;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace GlobalUCP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        // var emotionKey = "3ea63bdad6ec41659989eaa0d260a73b";
+        //var media = Plugin.Media.CrossMedia.Current;
+        //await media.Initialize();
+        //var file = await media.TakePhotoAsync(new StoreCameraMediaOptions { SaveToAlbum = false });
+
+        //Img.Source = ImageSource.FromStream(() => file.GetStream());
+
+
+        //var emotionclient = new EmotionServiceClient(emotionKey);
+        //var result = await emotionclient.RecognizeAsync(file.GetStream());
+
+        //var score = result.FirstOrDefault().Scores.ToRankedList().First().Key;
+
+        //LblResult.Text = $"Your emotion is {score}";
+
+        //file.Dispose();
+
+        private async void BtnImage_OnClicked(object sender, EventArgs e)
+        {
+
+            var visionKey = "d5fdc78fad5b4cf98fce5df15146426d";
+
+            var media = Plugin.Media.CrossMedia.Current;
+            await media.Initialize();
+            var file = await media.TakePhotoAsync(new StoreCameraMediaOptions { SaveToAlbum = false });
+            Img.Source = ImageSource.FromStream(() => file.GetStream());
+            var visionclient = new VisionServiceClient(visionKey);
+            var result = await visionclient.DescribeAsync(file.GetStream());
+            LblResult.Text = result.Description.Captions.First().Text;
+
+            file.Dispose();
+
+        }
+    }
+}
